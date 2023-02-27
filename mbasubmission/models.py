9fc874688a89e46cfd9c45f9904573a1f8b03860
Brandon Tupiti <tupitibrandon@gmail.com>
2023-02-27 17:42:15 +1100

    created Application and Document models

--- mbasubmission/models.py
+++ mbasubmission/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.urls import reverse
+import uuid
 
-# Create your models here.
+
+class Application(models.Model):
+    """
+    Application model
+    """
+    class ApplicationState(models.TextChoices):
+        PENDING = "PENDING", "PENDING"
+        ACCEPTED = "ACCEPTED", "ACCEPTED"
+        REJECTED = "REJECTED", "REJECTED"
+        
+    email = models.EmailField(max_length=254)
+    phone_number = models.IntegerField(null=True,blank=True)
+    first_name = models.CharField(max_length=254)
+    middle_name = models.CharField(max_length=254, null=True, blank=True)
+    last_name = models.CharField(max_length=254)
+    photo = models.ImageField()
+    application_state = models.CharField(max_length=8, choices=ApplicationState.choices, default=ApplicationState.PENDING)
+    
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.id
+    
+    def get_absolute_url(self):
+        return reverse("application-detail", kwargs={"pk": self.pk})
+ 
+ 
+    
+class Document(models.Model):
+    """
+    Document model
+    """
+    name = models.CharField(max_length=254, null=True, blank=True)
+    file = models.FileField()
+    application = models.ForeignKey(Application, on_delete=models.CASCADE, related_name='documents')
+    
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)   
+    
+    def __str__(self):
+        return self.id
+
+    
