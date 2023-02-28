54417abf771dac04caffa2f53dc52b05e85f4b67
Brandon Tupiti <tupitibrandon@gmail.com>
2023-02-28 12:29:06 +1100

    added view for handling GET and POST of the submission form

--- mbasubmission/views.py
+++ mbasubmission/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
+from .forms import ApplicationForm, DocumentForm
+from django.forms import formset_factory
 
 def submission_form(request):
+    """
+    displays a form for the user to submit an application
+    """
+    is_submitted = False
     
-    context = {}
+    DocumentFormSet = formset_factory(DocumentForm, extra=1)
+    document_formset = DocumentFormSet(prefix='documents')
+    application_form = ApplicationForm()
+    
+    if request.method == 'POST':
+        is_submitted = True
+    
+    context = {
+        'application_form':application_form, 
+        'document_formset': document_formset,
+        'is_submitted': is_submitted,
+    }
+    
     return render(request, 'mbasubmission/submission_form.html',context)
