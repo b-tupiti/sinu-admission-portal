1f93ad528bfb46b9bd52a996415fe64c8786fefe
Brandon Tupiti <tupitibrandon@gmail.com>
2023-03-05 17:59:16 +1100

    added views for users templates

--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
-# Create your views here.
+def LoginUser(request):
+    if request.method == 'POST':
+        return redirect('dashboard')
+    context = {}
+    return render(request, 'users/login.html', context)
+
+def Dashboard(request):
+    context = {}
+    return render(request, 'users/dashboard.html', context)
+
+def Applications(request):
+    context = {}
+    return render(request, 'users/applications.html', context)
+
+def ApplicationDetail(request):
+    context = {}
+    return render(request, 'users/application_detail.html', context)
