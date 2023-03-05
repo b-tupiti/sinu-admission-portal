fc0ef5eb5ef9cb641558aecdc61b92ca5522459e
Brandon Tupiti <tupitibrandon@gmail.com>
2023-03-06 10:45:58 +1100

    updated urls.py

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,8 @@ urlpatterns = [
     path('dashboard/applications/235532/', ApplicationDetail, name='application-detail'),
 ]
 
-
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
