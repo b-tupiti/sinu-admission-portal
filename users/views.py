91ea88773f547ead9119445d0cb5a318baea7677
Brandon Tupiti <tupitibrandon@gmail.com>
2023-03-07 09:58:41 +1100

    added user login functionality

--- users/views.py
+++ users/views.py
@@ -1,10 +1,38 @@
 from django.shortcuts import render,redirect
+from .models import User
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
 
 def LoginUser(request):
-    if request.method == 'POST':
+    
+    if request.user.is_authenticated:
         return redirect('dashboard')
-    context = {}
-    return render(request, 'users/login.html', context)
+    
+    if request.method == 'POST':
+        
+        email = request.POST['email']
+        password = request.POST['password']
+        
+        try:
+            user = User.objects.get(email=email)
+        except:
+            messages.error(request,'this email does not exist')
+
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect(request.GET['next'] if 'next' in request.GET else 'dashboard')
+
+        else:
+            messages.error(request,'your email or password is invalid')
+    
+    return render(request, 'users/login.html')
+
+
+
+
+
 
 def Dashboard(request):
     context = {}
