2b2a3ac130839b1b1341575272dd8e04fe92ab97
Brandon Tupiti <tupitibrandon@gmail.com>
2023-03-07 16:54:18 +1100

    updated STATICFILES_DIRS

--- config/settings.py
+++ config/settings.py
@@ -118,7 +118,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / 'static',
+    BASE_DIR / 'media/photos/',
 ]
 
 MEDIA_URL = 'media/'
