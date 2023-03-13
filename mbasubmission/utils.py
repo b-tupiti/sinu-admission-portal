4874b79cccddf6a3011a0cbb54ee853a5231ce50
Brandon Tupiti <tupitibrandon@gmail.com>
2023-03-13 13:50:53 +1100

    updated get_totals() method

--- mbasubmission/utils.py
+++ mbasubmission/utils.py
@@ -2,14 +2,21 @@ from .models import Application
 
 def get_totals():
     
-    total = Application.objects.all().count
-    accepted = Application.objects.filter(application_state=Application.ApplicationState.ACCEPTED).count
-    rejected = Application.objects.filter(application_state=Application.ApplicationState.REJECTED).count
+    total = Application.objects.all().count()
+    pending = Application.objects.filter(application_state=Application.ApplicationState.PENDING).count()
+    under_assessment = Application.objects.filter(application_state=Application.ApplicationState.UNDER_ASSESSMENT).count()
+    offer_letter_issued = Application.objects.filter(application_state=Application.ApplicationState.OFFER_LETTER_ISSUED).count()
+    cleared_for_enrolment = Application.objects.filter(application_state=Application.ApplicationState.CLEARED_FOR_ENROLLMENT).count()
+    enrolment_complete = Application.objects.filter(application_state=Application.ApplicationState.ENROLLMENT_COMPLETE).count()
     
     totals = {
         'total': total,
-        'accepted':accepted,
-        'rejected': rejected,
+        'in_process': total - enrolment_complete,
+        'pending':pending,
+        'under_assessment': under_assessment,
+        'offer_letter_issued': offer_letter_issued,
+        'cleared_for_enrolment': cleared_for_enrolment,
+        'enrolment_complete': enrolment_complete,
     }
     
     return totals
