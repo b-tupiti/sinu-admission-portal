49e761a37b5e7a83bb47c378bd4dabf28acf573b
Brandon Tupiti <tupitibrandon@gmail.com>
2023-03-14 15:06:31 +1100

    updated filter_applications function

--- users/utils.py
+++ users/utils.py
@@ -5,30 +5,31 @@ from django.contrib.auth.models import Group
 def filter_applications(request, filter):
     
     applications = Application.objects.all()
-    group = None
     
     #check if get request contains filter
     if filter is None:
         
-        # default filtering for each group
-        if request.user.groups.filter(pk=Group.objects.get(name="Assesors").pk).exists():
-            group = 'Assesors'
-            applications = Application.objects.filter(
-                Q(application_state=Application.ApplicationState.UNDER_ASSESSMENT) 
-                )
-        elif request.user.groups.filter(pk=Group.objects.get(name="Finance Department").pk).exists():
-            group = 'Finance Department'
-            applications = Application.objects.filter(
-                Q(application_state=Application.ApplicationState.OFFER_LETTER_ISSUED) 
-                )
-        elif request.user.groups.filter(pk=Group.objects.get(name="Student Administration Services (SAS) Department").pk).exists():
-            group = 'Student Administration Services (SAS) Department'
-            applications = Application.objects.filter(
-                Q(application_state=Application.ApplicationState.PENDING) |  
-                Q(application_state=Application.ApplicationState.CLEARED_FOR_ENROLLMENT)
-                )
-        else:
-            applications = Application.objects.all()
+        try:
+            if request.user.groups.filter(pk=Group.objects.get(name="Assesors").pk).exists():
+                group = 'Assesors'
+                applications = Application.objects.filter(
+                    Q(application_state=Application.ApplicationState.UNDER_ASSESSMENT) 
+                    )
+            elif request.user.groups.filter(pk=Group.objects.get(name="Finance Department").pk).exists():
+                group = 'Finance Department'
+                applications = Application.objects.filter(
+                    Q(application_state=Application.ApplicationState.OFFER_LETTER_ISSUED) 
+                    )
+            elif request.user.groups.filter(pk=Group.objects.get(name="Student Administration Services (SAS) Department").pk).exists():
+                group = 'Student Administration Services (SAS) Department'
+                applications = Application.objects.filter(
+                    Q(application_state=Application.ApplicationState.PENDING) |  
+                    Q(application_state=Application.ApplicationState.CLEARED_FOR_ENROLLMENT)
+                    )
+            else:
+                applications = Application.objects.all()
+        except:
+            group = None
     else:
         if filter == 'all_verbose':
             applications = Application.objects.all()
