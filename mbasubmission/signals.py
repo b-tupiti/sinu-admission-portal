485ae35a4b212c4e9d166f14726361f7a05341c3
Brandon Tupiti <tupitibrandon@gmail.com>
2023-03-16 13:57:56 +1100

    modified signal to send letter of offer email

--- mbasubmission/signals.py
+++ mbasubmission/signals.py
@@ -6,6 +6,8 @@ from django.dispatch import receiver
 import time
 import threading
 from django.template.loader import render_to_string
+import uuid
+from django.urls import reverse
 
 @receiver(post_save, sender=Application)
 def send_confirmation_email(sender, instance, created, **kwargs):
@@ -25,6 +27,28 @@ def send_confirmation_email(sender, instance, created, **kwargs):
                 fail_silently=True,
             )
         threading.Thread(target=send_email).start()
+    else:
+        if application.application_state == Application.ApplicationState.OFFER_LETTER_ISSUED:
+            
+            token = str(uuid.uuid4())
+            upload_url = f"{settings.BASE_URL}{reverse('upload_receipt')}?token={token}&id={application.id}"
+        
+            subject = "Congratulations! You have a provisional Offer Letter"
+            context = {'application': application, 'upload_url':upload_url}
+            html_message = render_to_string('mail/offer_issued_email.html', context)
+            def send_email():
+                send_mail(
+                    subject, 
+                    f'This email should include an Offer Letter, the proforma (invoice) for the MBA program, and SINU bank details so that the applicant can deposit.\nPlease click this link to upload your reciept: {upload_url}', 
+                    settings.EMAIL_HOST, 
+                    ['tupitibrandon@gmail.com'], 
+                    html_message=html_message,
+                    fail_silently=True,
+                )
+            threading.Thread(target=send_email).start()
+            
+           
+        
 
 
         
