a021524e2ef8c54550f030d6d6288483b3a25127
Brandon Tupiti <tupitibrandon@gmail.com>
2023-04-27 17:15:56 +1100

    added pagination and search to courses app

--- courses/views.py
+++ courses/views.py
@@ -1,23 +1,24 @@
 from django.shortcuts import render
 from .models import Course
+from .utils import searchCourses, paginateCourses
+
 
 def findcourse(request):
     
-    search = ''
-    if 'search' in request.GET:
-        search = request.GET['search']
-        courses = Course.objects.filter(title__contains=search)
-        context = {
-            'search': search,
-            'courses': courses,
-        }
-    else:
-        context = {
-            'search': search
-        }
+    courses, search = searchCourses(request)
+    courses_total = courses.count()
+    custom_range, courses, first_index, last_index = paginateCourses(request, courses, 5, courses_total)
+    
+    context = {
+        'courses':courses,
+        'courses_total': courses_total,
+        'search':search,
+        'custom_range': custom_range,
+        'first_index': first_index,
+        'last_index': last_index,
+    }
     
     return render(request, 'courses/find-a-course.html', context)
-
 
 
 
