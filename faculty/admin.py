0b744453e5c9fdb1fd7e7081aa96be1c9cbc255f
Brandon Tupiti <tupitibrandon@gmail.com>
2023-04-28 09:54:40 +1100

    registered faculty models to admin site

--- faculty/admin.py
+++ faculty/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Faculty, School, Department
 
-# Register your models here.
+class FacultyAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Faculty, FacultyAdmin)
+
+class SchoolAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(School,SchoolAdmin)
+
+class DepartmentAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Department, DepartmentAdmin)
