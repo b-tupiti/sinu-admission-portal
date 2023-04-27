f4e6ec4a4e19db13d5d9a84c3136fe8c059cf0ce
Brandon Tupiti <tupitibrandon@gmail.com>
2023-04-28 09:49:52 +1100

    created faculty app and models

---
+++ faculty/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class Faculty(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Name of Faculty')
+    abbr = models.CharField(max_length=10, verbose_name='Faculty Abbreviation')
+    
+    def __str__(self):
+        return ' - '.join[self.name, self.abbr]
+    
+    
+class School(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Name of School')
+    abbr = models.CharField(max_length=100, verbose_name='School Abbreviation')
+    faculty = models.ForeignKey(Faculty, on_delete=models.SET_NULL)
+    
+    def __str__(self):
+        return ' - '.join[self.name, self.abbr]
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Name of Department')
+    abbr = models.CharField(max_length=10, blank=True, null=True, verbose_name='Department Abbreviation')
+    school = models.ForeignKey(School, on_delete=models.SET_NULL)
+    
+    def __str__(self):
+        return ' - '.join[self.name, self.abbr]
+    
+
