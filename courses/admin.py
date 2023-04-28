00b019f278f14e01fff5bb77e043c7b6408e920b
Brandon Tupiti <tupitibrandon@gmail.com>
2023-04-28 18:14:19 +1100

    added Unit model to admin site

--- courses/admin.py
+++ courses/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Course
+from .models import Course, Unit
 
 class CourseAdmin(admin.ModelAdmin):
     pass
 admin.site.register(Course, CourseAdmin)
 
+class UnitAdmin(admin.ModelAdmin):
+    list_display = ('code','title')
+admin.site.register(Unit, UnitAdmin)
 
