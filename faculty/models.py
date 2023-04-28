c497bf558b6e5e18fd7b5e73a02e4d8463cc9ce5
Brandon Tupiti <tupitibrandon@gmail.com>
2023-04-28 12:15:07 +1100

    updated __str__ method for Department model

--- faculty/models.py
+++ faculty/models.py
@@ -26,6 +26,8 @@ class Department(models.Model):
     school = models.ForeignKey(School, on_delete=models.SET_NULL, null=True)
     
     def __str__(self):
-        return ' - '.join([self.name, self.abbr])
+        if self.abbr:
+            return ' - '.join([self.name, self.abbr])
+        return self.name
     
 
