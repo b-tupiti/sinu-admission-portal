4a8e6a903ca46094c76f9c2c3f69f5626651564d
Brandon Tupiti <tupitibrandon@gmail.com>
2023-04-28 12:22:08 +1100

    updated models.py

--- faculty/models.py
+++ faculty/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class Faculty(models.Model):
+    
+    class Meta:
+        verbose_name_plural = 'Faculties'
+        
     name = models.CharField(max_length=100, verbose_name='Name of Faculty')
     abbr = models.CharField(max_length=10, verbose_name='Faculty Abbreviation')
     
