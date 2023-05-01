1d291cc5b33c345eeccae1735a9649f34750cadc
Brandon Tupiti <tupitibrandon@gmail.com>
2023-05-01 23:16:53 +1100

    added model imports

--- courses/apps.py
+++ courses/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class CoursesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'courses'
+    
+    def ready(self):
+        from .models.course import Course
+        from .models.unit import Unit
+        from .models.courseunit import CourseUnit
+        from .models.studyperiod import StudyPeriod
