d56a457ead23cd0106146524c0c5e568b9d940dd
Brandon Tupiti <tupitibrandon@gmail.com>
2023-05-02 01:23:07 +1100

    updated CourseUnitAdmin

--- courses/admin.py
+++ courses/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
+from django import forms
+from django.db import models
 from .models.course import Course
 from .models.unit import Unit
 from .models.courseunit import CourseUnit
 from .models.studyperiod import StudyPeriod
+
+    
+class CourseUnitAdmin(admin.ModelAdmin):
+     def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "unit":
+            kwargs["queryset"] = Unit.objects.order_by("title")
+        if db_field.name == "course":
+            kwargs["queryset"] = Course.objects.order_by("title")
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+    
+admin.site.register(CourseUnit, CourseUnitAdmin)
 
 class CourseAdmin(admin.ModelAdmin):
     pass
@@ -12,9 +25,8 @@ class UnitAdmin(admin.ModelAdmin):
     list_display = ('code','title')
 admin.site.register(Unit, UnitAdmin)
 
-class CourseUnitAdmin(admin.ModelAdmin):
-    pass
-admin.site.register(CourseUnit, CourseUnitAdmin)
+
+
 
 class StudyPeriodAdmin(admin.ModelAdmin):
     pass
