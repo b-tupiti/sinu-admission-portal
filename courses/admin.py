afa635852bdef1dc9add8394d72d32f82a27edc3
Brandon Tupiti <tupitibrandon@gmail.com>
2023-05-01 23:32:32 +1100

    added CourseUnit and StudyPeriod to admin site

--- courses/admin.py
+++ courses/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models.course import Course
 from .models.unit import Unit
+from .models.courseunit import CourseUnit
+from .models.studyperiod import StudyPeriod
 
 class CourseAdmin(admin.ModelAdmin):
     pass
@@ -9,4 +11,12 @@ admin.site.register(Course, CourseAdmin)
 class UnitAdmin(admin.ModelAdmin):
     list_display = ('code','title')
 admin.site.register(Unit, UnitAdmin)
+
+class CourseUnitAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(CourseUnit, CourseUnitAdmin)
+
+class StudyPeriodAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(StudyPeriod, StudyPeriodAdmin)
 
