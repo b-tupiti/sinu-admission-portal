209a5f8adc26b18a4b89f1cdeef9c9ffab08f2c0
Brandon Tupiti <tupitibrandon@gmail.com>
2023-05-05 21:56:37 +1100

    customized admin site for Application

--- admission/admin.py
+++ admission/admin.py
@@ -3,5 +3,62 @@ from .models import Application
 
 
 class ApplicationAdmin(admin.ModelAdmin):
-    pass
+    fieldsets = (
+         ('Applicant', {
+            'fields': (
+                'applicant',
+            )
+        }),
+        ('Student ID (inserted by applicant if he/she is a former student)', {
+            'fields': (
+                'student_id',
+            )
+        }),
+        ('Personal Details', {
+            'fields': (
+                'photo', 
+                'title',
+                'first_name',
+                'middle_name',
+                'last_name',
+                'date_of_birth',
+                'gender',
+                'marital_status',
+                'phone_number',
+                'email',
+                'has_special_needs',
+            )
+        }),
+        ('Sponsor Details', {
+            'fields': (
+                'sponsor_type', 
+                'sponsor_name',
+                'sponsor_email',
+                'sponsor_phone_number',
+                'sponsor_address',
+            )
+        }),
+        ('Education Background', {
+            'fields': (
+                'third_form_school', 
+                'third_form_year',
+                'fifth_form_school',
+                'fifth_form_year',
+                'sixth_form_school',
+                'sixth_form_year',
+                'foundation_school',
+                'foundation_year',
+            ),
+        }),
+        ('Declaration', {
+            'fields': (
+                'is_declared', 
+            ),
+        }),
+        ('Meta', {
+            'fields': (
+                'application_state', 
+            ),
+        }),
+    )
 admin.site.register(Application, ApplicationAdmin)
