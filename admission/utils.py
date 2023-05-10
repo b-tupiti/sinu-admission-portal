a87a6eca403313f9d9a37f0fc0b5a5c528bf5240
Brandon Tupiti <tupitibrandon@gmail.com>
2023-05-10 15:51:23 +1100

    added new util function for application creation

--- admission/utils.py
+++ admission/utils.py
@@ -1,22 +1,38 @@
-from .models import Application
+from .models import ApplicationState, Application
+
+
+def create_new_admission_application_for_user(user, course):
+    application = Application.objects.create(
+                applicant=user,
+                selected_course=course,
+                first_name=user.first_name,
+                middle_name=user.middle_name,
+                last_name=user.last_name,
+                date_of_birth=user.date_of_birth,
+                gender=user.gender,
+            )
+    return application
+
+
+
+
+
 
 def get_totals():
     
     total = Application.objects.all().count()
-    pending = Application.objects.filter(application_state=Application.ApplicationState.PENDING).count()
-    under_assessment = Application.objects.filter(application_state=Application.ApplicationState.UNDER_ASSESSMENT).count()
-    offer_letter_issued = Application.objects.filter(application_state=Application.ApplicationState.OFFER_LETTER_ISSUED).count()
-    cleared_for_enrolment = Application.objects.filter(application_state=Application.ApplicationState.CLEARED_FOR_ENROLLMENT).count()
-    enrolment_complete = Application.objects.filter(application_state=Application.ApplicationState.ENROLLMENT_COMPLETE).count()
+    pending = Application.objects.filter(application_state=ApplicationState.SUBMITTED).count()
+    under_assessment = Application.objects.filter(application_state=ApplicationState.DRAFT).count()
+
     
     totals = {
         'total': total,
-        'in_process': total - enrolment_complete,
-        'pending':pending,
-        'under_assessment': under_assessment,
-        'offer_letter_issued': offer_letter_issued,
-        'cleared_for_enrolment': cleared_for_enrolment,
-        'enrolment_complete': enrolment_complete,
+        # 'in_process': total - enrolment_complete,
+        # 'pending':pending,
+        # 'under_assessment': under_assessment,
+        # 'offer_letter_issued': offer_letter_issued,
+        # 'cleared_for_enrolment': cleared_for_enrolment,
+        # 'enrolment_complete': enrolment_complete,
     }
     
     return totals
