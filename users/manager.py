3afd687d10ec41a1e418d1763b1040526a077ae7
Brandon Tupiti <tupitibrandon@gmail.com>
2023-05-10 15:49:18 +1100

    updated User manager to include extra required fields

--- users/manager.py
+++ users/manager.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 class UserManager(BaseUserManager):
 
-  def _create_user(self, email, password, is_staff, is_superuser, **extra_fields):
+  def _create_user(self, email, password, is_staff, is_superuser, first_name, middle_name, last_name, gender, date_of_birth, **extra_fields):
     if not email:
         raise ValueError('Users must have an email address')
     now = timezone.now()
@@ -13,6 +13,11 @@ class UserManager(BaseUserManager):
         is_staff=is_staff, 
         is_active=True,
         is_superuser=is_superuser, 
+        first_name=first_name,
+        middle_name=middle_name,
+        last_name=last_name,
+        gender=gender,
+        date_of_birth=date_of_birth,
         last_login=now,
         date_joined=now, 
         **extra_fields
@@ -21,8 +26,29 @@ class UserManager(BaseUserManager):
     user.save(using=self._db)
     return user
 
-  def create_user(self, email, password, **extra_fields):
-    return self._create_user(email, password, False, False, **extra_fields)
+  def create_user(
+    self, 
+    email, 
+    password, 
+    first_name, 
+    middle_name, 
+    last_name, 
+    gender,
+    date_of_birth,
+    **extra_fields
+    ):
+    return self._create_user(
+      email, 
+      password, 
+      False, 
+      False,
+      first_name, 
+      middle_name, 
+      last_name, 
+      gender,
+      date_of_birth, 
+      **extra_fields
+    )
 
   def create_superuser(self, email, password, **extra_fields):
     user=self._create_user(email, password, True, True, **extra_fields)
