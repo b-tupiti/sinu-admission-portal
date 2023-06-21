0d43b34e065236abef9775692d3a379369a1639e
Brandon Ryan Tupiti <tupitibrandon@gmail.com>
2023-06-21 15:53:52 +1100

    collected staticfiles

--- staticfiles/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.findcourse, name="find-course"),
-    path('course-detail/<str:code>/', views.course, name="course-detail"),
-  
-]
