43597b2bee844b730ef13ce801286dfeb32f34bb
Brandon Ryan Tupiti <tupitibrandon@gmail.com>
2023-06-30 22:24:59 +1100

    updated urls.py

--- admission/urls.py
+++ admission/urls.py
@@ -1,17 +1,9 @@
 
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.submission_form, name="submission-form"), # old 
-    # path('upload-my-deposit-slip/', views.upload_deposit_slip, name='upload-my-deposit-slip'), # old
-    path('new-applicant/', views.create_new_admission, name="create-new-admission"),
-    path('application/saved/', views.application_saved, name="application-saved"),
-    path('application/<str:pk>/personal-details/', views.personal_details, name="personal-details"),
-    path('application/<str:pk>/sponsor-details/', views.sponsor_details, name="sponsor-details"),
-    path('application/<str:pk>/education-background/', views.education_background, name="education-background"),
-    path('application/<str:pk>/employment-history/', views.employment_history, name="employment-history"),
-    path('application/<str:pk>/declaration/', views.declaration, name="declaration"),
-    path('my-admissions/', views.my_admissions, name="my-admissions"),
+    path('new-application/', views.create_new_application, name="create-new-application"),
+    path('application/<str:pk>/saved/', views.application_saved, name="application-saved"),
+    path('application/<str:pk>/', views.application, name="application"),
 ]
