4bbc6517c08486114b2824b0f119904024816ee6
Brandon Ryan Tupiti <tupitibrandon@gmail.com>
2023-07-19 22:54:19 +1100

    added SponsorshipLetter model to admin site

--- admission/admin.py
+++ admission/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from .models import Application
+from .models import Application, SponsorshipLetter
+
+
+
+class SponsorshipLetterAdmin(admin.ModelAdmin):
+    list_display=('file','application')
+
+
+admin.site.register(SponsorshipLetter, SponsorshipLetterAdmin)
 
 
 class ApplicationAdmin(admin.ModelAdmin):
