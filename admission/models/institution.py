debdf4e007791e8b172f33a4f7ef4e2eadd19768
Brandon Ryan Tupiti <tupitibrandon@gmail.com>
2023-07-25 08:38:41 +1100

    added Institution model, made migrations

---
+++ admission/models/institution.py
@@ -0,0 +1,43 @@
+from django.db import models
+from .application import Application
+
+
+class Institution(models.Model):
+    application = models.ForeignKey(
+        Application,
+        related_name='institutions',
+        on_delete=models.CASCADE,
+    )
+    
+    name = models.CharField(
+        max_length=255,
+        verbose_name='Instition',
+        null=True,
+        blank=True,
+    )
+    
+    course = models.CharField(
+        max_length=255,
+        verbose_name='Course / Qualification',
+        null=True,
+        blank=True,
+    )
+    
+    year_start = models.IntegerField(
+        verbose_name='Year Started',
+        null=True,
+        blank=True,
+    )
+    
+    year_end = models.IntegerField(
+        verbose_name='Year Ended',
+        null=True,
+        blank=True,
+    )
+    
+    major = models.CharField(
+        max_length=255,
+        verbose_name='Major Field of Study',
+        null=True,
+        blank=True,
+    )
