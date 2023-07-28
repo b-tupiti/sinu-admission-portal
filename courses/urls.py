4b03a91cc334476b84e8b3f8117df383b7081a86
Brandon Ryan Tupiti <tupitibrandon@gmail.com>
2023-07-29 07:36:38 +1100

    renamed url name search-courses to course-search

--- courses/urls.py
+++ courses/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.search_courses, name="search-courses"),
+    path('', views.search_courses, name="course-search"),
     path('course-detail/<str:code>/', views.course, name="course-detail"),
   
 ]
