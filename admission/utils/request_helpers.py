f8a329e1ff3522779808979c01a26e917d6eb00b
Brandon Ryan Tupiti <tupitibrandon@gmail.com>
2023-07-29 12:38:03 +1100

    implemented function to check if a GET param has no value, or does not exist at all. Intended for url redirections.

--- admission/utils/request_helpers.py
+++ admission/utils/request_helpers.py
@@ -1,6 +1,12 @@
-
 def is_put_request(request):
     """
-    checks if the request object actually simulates a PUT request.
+    checks if the request object simulates a PUT request.
     """
     return request.method == 'POST' and request.POST.get('_method') == 'put'
+
+
+def param_not_found_or_empty(param):
+    """
+    checks if param does not exist, or does but is empty.
+    """
+    return not param
