727fee0847c3ee9493bd134a9c8c35868a2c2022
Brandon Ryan Tupiti <tupitibrandon@gmail.com>
2023-07-29 12:35:01 +1100

    deleted retrieve_course.py file.

--- admission/utils/retrieve_course.py
+++
@@ -1,6 +0,0 @@
-from courses.models.course import Course
-
-def get_course_from_code(request):
-    course_code = request.GET.get('course_code')
-    course =  Course.objects.values('code', 'title', 'campus').get(code=course_code)
-    return course
