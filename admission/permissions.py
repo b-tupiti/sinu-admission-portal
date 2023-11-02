9c58f01aa20c50c73fe56d194cc0671aec8cc1d0
Brandon Ryan Tupiti <tupitibrandon@gmail.com>
2023-11-02 20:12:34 +1100

    added pdf templates.

--- admission/permissions.py
+++ admission/permissions.py
@@ -1,5 +1,6 @@
 from guardian.shortcuts import remove_perm, assign_perm
 from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import user_passes_test
 
 class ObjectPermissionsManager():
     
@@ -18,5 +19,21 @@ class ObjectPermissionsManager():
             group, created = Group.objects.get_or_create(name=group_name)
             for perm in permissions:
                 assign_perm(perm, group, self.obj)
+                
+def permission_required(permission_name, login_url=None, raise_exception=False):
+    """
+    Custom decorator to check for a specific permission.
+    :param permission_name: The name of the permission to check.
+    :param login_url: URL to redirect to if the user is not authenticated.
+    :param raise_exception: If True, raise a PermissionDenied exception on failure.
+    """
+    def check_permission(user):
+        return user.has_perm(permission_name)
+
+    return user_passes_test(
+        check_permission,
+        login_url=login_url,
+        raise_exception=raise_exception
+    )
 
         
