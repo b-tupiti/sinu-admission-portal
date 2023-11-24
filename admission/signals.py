66b4118c5fc2595e96c6a351852aac15a08ae9cf
Brandon Ryan Tupiti <tupitibrandon@gmail.com>
2023-11-25 01:56:07 +1100

    updated admission signals.

--- admission/signals.py
+++ admission/signals.py
@@ -9,6 +9,7 @@ from admission.utils.admission_emails import (
 from .models.application import Application
 from django.db.models.signals import (
     pre_save,
+    post_save,
 )
 
 @receiver(pre_save, sender=Application)
@@ -16,6 +17,10 @@ def before_instance_is_saved(sender, instance, **kwargs):
     
     if not instance._state.adding:
         set_permissions_based_on_application_status(instance)
-        send_email_based_on_application_status(instance)
+        
     else:
         send_application_created_email(instance)
+        
+@receiver(post_save, sender=Application)
+def after_instance_is_saved(sender, instance, **kwargs):
+    send_email_based_on_application_status(instance)
