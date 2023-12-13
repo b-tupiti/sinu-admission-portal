0510753d07377d47e0c3b8722c86be75fcd0d6d7
Brandon Ryan Tupiti <tupitibrandon@gmail.com>
2023-12-14 02:24:29 +1100

    collected static files

--- staticfiles/js/login-validation.js
+++ staticfiles/js/login-validation.js
@@ -1,197 +1,147 @@
-
-
-// if all inputs are valid, save and continue button will be enabled.
-const inputs = {
-    'validEmail': false,
-    'confirmEmail': false,
-    'validPassword': false,
-    'confirmPassword': false,
+const InputVal = {
+    EMAIL: false,
+    CONFIRMEMAIL: false,
+    PASSWORD: false,
+    CONFIRMPASS: false,
 }
 
-const emailInput = document.getElementById('email');
-const confirmEmailInput = document.getElementById('confirm-email');
-const passwordInput = document.getElementById('password');
-const confirmPasswordInput = document.getElementById('confirm-password');
-const emailInfo = document.getElementById('validate-email-check');
-const passwordInfo = document.getElementById('validate-password-check')
-const confirmEmailInfo = document.getElementById('confirm-email-check');;
-const confirmPasswordInfo = document.getElementById('confirm-password-check');
+const Input = {
+    EMAIL: 'email',
+    CONFIRMEMAIL: 'confirmemail',
+    PASSWORD: 'password',
+    CONFIRMPASS: 'confirmpass',
+}
+
+const saveContinueBtn = document.getElementById('save-and-continue');
+
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#@$!%*?&])[A-Za-z\d#@$!%*?&]{8,}$/;
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
+
+const email = document.getElementById('email');
+const confirmEmail = document.getElementById('confirm-email');
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirm-password');
+
+const emailIconDiv = document.getElementById('validate-email-check');
+const confirmEmailIconDiv = document.getElementById('confirm-email-check');
+const passwordIconDiv = document.getElementById('validate-password-check');
+const confirmPasswordIconDiv = document.getElementById('confirm-password-check');
+
+email.addEventListener('keyup', function(e){keyEnteredHandler(e.target.value, Input.EMAIL)});
+confirmEmail.addEventListener('keyup', function(e){keyEnteredHandler(e.target.value, Input.CONFIRMEMAIL)});
+password.addEventListener('keyup', function(e){keyEnteredHandler(e.target.value, Input.PASSWORD)});
+confirmPassword.addEventListener('keyup', function(e){keyEnteredHandler(e.target.value, Input.CONFIRMPASS)});
 
 
-document.addEventListener('DOMContentLoaded', function() {
-    if(emailInput.value != '')
-        emailValidityCheck(emailInput.value);
-    if(confirmEmailInput.value != '')
-        confirmEmailCheck(confirmEmailInput.value, emailInput.value);
-});
+document.addEventListener('DOMContentLoaded', function() { 
+    setTimeout(function(){
+       loginInit();
+    }, 200);
+  });
 
-emailInput.addEventListener('keyup', function(){
 
-    emailValidityCheck(this.value);
-    
-    if(confirmEmailInput.value)
-        confirmEmailCheck(this.value, confirmEmailInput.value);
-})
+function loginInit(){
 
-confirmEmailInput.addEventListener('keydown', function(e){
-    if (e.key === 'Tab' || e.key === 'Enter') {
-        return;
+    InputVal.PASSWORD = false;
+    InputVal.CONFIRMPASS = false;
+
+    if(email.value !== '') {
+        InputVal.EMAIL = emailRegex.test(email.value);
+        updateIconDiv(InputVal.EMAIL, emailIconDiv);
     }
-    confirmEmailCheck(this.value + e.key, emailInput.value);
-});
-
-passwordInput.addEventListener('keyup', function(){
-    passwordValidityCheck(this.value);
-
-    if(confirmPasswordInput.value)
-        confirmPasswordCheck(this.value, confirmPasswordInput.value);
-});
-
-confirmPasswordInput.addEventListener('keydown', function(e){
-    if (e.key === 'Tab' || e.key === 'Enter') {
-        return;
+    if(confirmEmail.value !== ''){
+        InputVal.CONFIRMEMAIL = valuesMatch(confirmEmail.value, email.value);
+        updateIconDiv(InputVal.CONFIRMEMAIL, confirmEmailIconDiv);
     }
-    confirmPasswordCheck(this.value + e.key, passwordInput.value);
-});
 
-document.addEventListener('keydown', function(){
-    
-    if(allInputsValid())
-        document.getElementById('save-and-continue').removeAttribute('disabled');
+    if(allValidInputs(InputVal))
+        saveContinueBtn.removeAttribute('disabled');
     else 
-        document.getElementById('save-and-continue').setAttribute('disabled', true);
-    
-});
+        saveContinueBtn.setAttribute('disabled', true);
 
-function allInputsValid(){
-    
-    let inputValid = true;
+}
 
-    Object.keys(inputs).map(key => {
 
-        if(inputs[key] == false){
-            inputValid = false;
+function keyEnteredHandler(value, inputType){
+
+    switch(inputType){
+        
+        case 'email':
+            InputVal.EMAIL = emailRegex.test(value);
+            updateIconDiv(InputVal.EMAIL, emailIconDiv);
+            InputVal.CONFIRMEMAIL = valuesMatch(value, confirmEmail.value);
+            updateIconDiv(InputVal.CONFIRMEMAIL, confirmEmailIconDiv);
+            break;
+
+        case 'confirmemail':
+            InputVal.CONFIRMEMAIL = valuesMatch(value, email.value);
+            updateIconDiv(InputVal.CONFIRMEMAIL, confirmEmailIconDiv);
+            break;
+
+        case 'password':
+            InputVal.PASSWORD = passwordRegex.test(value);
+            updateIconDiv(InputVal.PASSWORD, passwordIconDiv);
+            InputVal.CONFIRMPASS = valuesMatch(value, confirmPassword.value);
+            updateIconDiv(InputVal.CONFIRMPASS, confirmPasswordIconDiv);
+            break;
+
+        case 'confirmpass':
+            InputVal.CONFIRMPASS = valuesMatch(value, password.value);
+            updateIconDiv(InputVal.CONFIRMPASS, confirmPasswordIconDiv);
+            break;
+    }
+
+
+    if(allValidInputs(InputVal))
+        saveContinueBtn.removeAttribute('disabled');
+    else 
+        saveContinueBtn.setAttribute('disabled', true);
+
+
+    console.clear();
+    console.log(`email: ${email.value}`)
+    console.log(`confirm email: ${confirmEmail.value}`)
+    console.log(`password: ${password.value}`)
+    console.log(`confirm password: ${confirmPassword.value}`)
+    console.log(InputVal);
+
+}
+
+function valuesMatch(value, valueToMatch){
+    if(value)
+        return value === valueToMatch;
+    return false;
+}
+
+
+function updateIconDiv(isValid, IconDiv){
+    if (isValid){
+        IconDiv.innerHTML = `
+            <span class="has-text-success ">
+                <i class="fa-solid fa-check mr-3"></i>
+            </span>`
+    }
+    else {
+        IconDiv.innerHTML = `
+            <span class="has-text-danger ">
+                <i class="fa-solid fa-x mr-3"></i>
+            </span>`
+    }
+}
+
+function allValidInputs(valInputs){
+    let allValid = true;
+
+    Object.keys(valInputs).map(key => {
+
+        if(valInputs[key] == false){
+            allValid = false;
             return;
         }
 
     })
 
-    return inputValid;
+    return allValid;
 }
 
-
-function passwordValidityCheck(password){
-    
-    passwordInfo.innerHTML = '';
-
-    res = isValidPassword(password);
-
-    if(res['isValid'] === true){
-        passwordInfo.innerHTML = `
-        <span class="has-text-success ">
-            <i class="fa-solid fa-check mr-3"></i>
-        </span>`
-        inputs['validPassword'] = true;
-    }
-    else if (res['isValid'] === false) {
-        passwordInfo.innerHTML = `
-        <span class="has-text-danger">
-            <i class="fa-solid fa-x mr-3"></i>
-            <small style="font-size:10px">
-            </small>
-        </span>`
-        inputs['validPassword'] = false;
-    }
-}
-
-function isValidPassword(password){
-
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#@$!%*?&])[A-Za-z\d#@$!%*?&]{8,}$/;
-
-    if (!passwordRegex.test(password)) {
-        const requirements = [
-            {'rule': /[a-z]/, 'desc': 'at least 1 lower case letter'}, 
-            {'rule': /[A-Z]/, 'desc': 'at least 1 upper case letter'}, 
-            {'rule': /\d/, 'desc': 'at least 1 numeric value'},   
-            {'rule': /[#@$!%*?&]/, 'desc': 'at least one special character (#@$!%*?&)'}, 
-            {'rule': /.{8,}/, 'desc': 'at least 8 characters long'}  
-        ];
-
-        const message = "Password must meet the following criteria:" +
-            requirements.map((requirement) => `${requirement['rule'].test(password) ? '' : requirement['desc']}`);
-
-        return { isValid: false, message: message };
-    }
-
-    inputs['validPassword'] = true;
-    return { isValid: true, message: "Password is valid." };
-}
-
-
-function emailValidityCheck(email){
-    
-    emailInfo.innerHTML = '';
-    if(isValidEmail(email)){
-        emailInfo.innerHTML = `
-            <span class="has-text-success ">
-                <i class="fa-solid fa-check mr-3"></i>
-            </span>`
-        inputs['validEmail'] = true;
-    }
-    else {
-        emailInfo.innerHTML = `
-            <span class="has-text-danger ">
-                <i class="fa-solid fa-x mr-3"></i>
-            </span>`
-        inputs['validEmail'] = false;
-    }
-}
-
-
-function isValidEmail(email) {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-}
-
-function confirmEmailCheck(email, confirmEmail){
-    confirmEmailInfo.innerHTML = '';
-
-    if (valuesMatch(email, confirmEmail)){
-        confirmEmailInfo.innerHTML = `
-        <span class="has-text-success ">
-            <i class="fa-solid fa-check mr-3"></i>
-        </span>`
-        inputs['confirmEmail'] = true;
-    }
-    else {
-        confirmEmailInfo.innerHTML = `
-        <span class="has-text-danger ">
-            <i class="fa-solid fa-x mr-3"></i>
-        </span>`
-        inputs['confirmEmail'] = false;
-    }
-}
-
-function confirmPasswordCheck(password, confirmPassword){
-    confirmPasswordInfo.innerHTML = '';
-
-    if (valuesMatch(password, confirmPassword)){
-        confirmPasswordInfo.innerHTML = `
-        <span class="has-text-success ">
-            <i class="fa-solid fa-check mr-3"></i>
-        </span>`
-        inputs['confirmPassword'] = true;
-    }
-    else {
-        confirmPasswordInfo.innerHTML = `
-        <span class="has-text-danger ">
-            <i class="fa-solid fa-x mr-3"></i>
-        </span>`
-        inputs['confirmPassword'] = false;
-    }
-}
-
-function valuesMatch(value, valueToMatch){
-    return value === valueToMatch;
-}
 
